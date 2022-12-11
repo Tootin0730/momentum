d0a52abd607cef3a65604c1376f0b6423f589f61
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-10 23:03:49 -0500

    const and let
    What can't change and what can change.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const a = 5;
-const b = 2;
-const myName = "san";
+let a = 5;
+let b = 2;
+let myName = "san";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "mountain";
+console.log("your new name is " + myName);
