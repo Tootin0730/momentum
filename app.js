f338ec05d3a38bc09597684e0988a912e50c3b6a
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-15 10:15:55 -0500

    Conditionals
    Learned about function "pormpt" and function "parseInt"

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-const age = 96;
+const age = parseInt(prompt("How old are you?"));
 
-function calculatKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
-
-const krAge = calculatKrAge(age);
-
-console.log(krAge);
-
-//Make calculator using "return"//
-
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+console.log(age);
 
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
 
 
 
