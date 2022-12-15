cc6c77dbb7eea1fea3cca92ff5efe64e9cd64850
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-15 10:38:34 -0500

    Conditional part two
    Learned about functions "isNaN", "if", and "else".

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const age = parseInt(prompt("How old are you?"));
 
 console.log(age);
 
+console.log(isNaN(age));
+
+if(isNaN(age)) {
+  console.log("Please write a number.");
+}
+else {
+  console.log("Thank you for writing your age.");
+}
+
 
 
 
