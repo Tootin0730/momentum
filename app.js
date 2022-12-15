8066888c4475182ee47286b1b6ae6a00fe0b6f7a
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-15 11:42:50 -0500

    Conditional part three
    Learned about "else if" "&&" "||".
    And Created a calculator to see if people could drink.

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ console.log(age);
 
 console.log(isNaN(age));
 
-if(isNaN(age)) {
-  console.log("Please write a number.");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number.");
 }
-else {
-  console.log("Thank you for writing your age.");
+else if (age < 18) {
+  console.log("You are too young.");
 }
+else if (age >= 18 && age <=50) {
+  console.log("You can drink.");
+}
+else if (age > 50 && age <=80) {
+  console.log("You should exercise.");
+}
+else if (age === 100) {
+  console.log("Wow! You must be wise!");
+}
+else if (age > 80) {
+  console.log("You can do whatever you want.");
+}
+
 
 
 
