00eb15e854ef339064ea512a15be9f5cd7c900d7
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-15 15:38:05 -0500

    HTML in JavaScript
    To bring something from HTML and add something to HTML.

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-console.log(age);
+title.innerText = "Got you!";
 
-console.log(isNaN(age));
+console.log(title.id);
+console.log(title.className);
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number.");
-}
-else if (age < 18) {
-  console.log("You are too young.");
-}
-else if (age >= 18 && age <=50) {
-  console.log("You can drink.");
-}
-else if (age > 50 && age <=80) {
-  console.log("You should exercise.");
-}
-else if (age === 100) {
-  console.log("Wow! You must be wise!");
-}
-else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
 
 
 
