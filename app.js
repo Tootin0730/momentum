ab03712be76d1313798ed29d9e830e5cbf10e558
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-16 10:52:46 -0500

    Input values
    Bring the "Input value" Using property "value".

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+  console.log(loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
-
-
+loginButton.addEventListener("click", onLoginBtnClick);
 
 
 
