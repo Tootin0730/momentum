7dd4cc992534bbe415c02243e6e04edf4d972121
Tootin0730 <YISAN1004@GMAIL.COM>
2022-12-16 17:30:36 -0500

    Saving username
    How to save a username to local storage. => "localStorage.setItem"
    Also, "localStorage.getItem", "localStorage.removeItem"

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}. Welcome!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
